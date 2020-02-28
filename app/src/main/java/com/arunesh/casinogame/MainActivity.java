f2d6f2931f2d9d19309778319d0682bc8f608454
Arunesh <arunesh447@gmail.com>
2020-02-28 23:43:25 +0530

    Initial commit

---
+++ app/src/main/java/com/arunesh/casinogame/MainActivity.java
@@ -0,0 +1,32 @@
+package com.arunesh.casinogame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button rollButton;
+        rollButton = (Button) findViewById(R.id.rollbutton);
+
+        ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice2);
+        ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Click","Button has been pressed");
+            }
+        });
+
+    }
+}
